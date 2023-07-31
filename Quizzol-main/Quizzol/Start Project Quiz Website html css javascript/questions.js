e5749f5959a49f5a77fe20d9c21dcc4e51510a90
Bhavya Hingad <102456511+bhavya1606@users.noreply.github.com>
2023-07-31 19:47:41 +0530

    Add files via upload

---
+++ Quizzol-main/Quizzol/Start Project Quiz Website html css javascript/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "In which city did the first modern Olympic Games take place in 1896?",
+        answer: "A. Athens, Greece",
+        options: [
+            "A. Athens, Greece",
+            "B. Rome, Italy",
+            "C. Paris, France",
+            "D. London, United Kingdom,"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which country has won the most gold medals in the history of the Summer Olympics?",
+        answer: "A. United States",
+        options: [
+            "A. United States",
+            "B. China",
+            "C. Soviet Union",
+            "D. Germany"
+        ]
+    },
+    {
+        numb: 3,
+        question: "The Olympic motto is Citius, Altius, Fortius which means:",
+        answer: "B. Faster, Higher, Stronger",
+        options: [
+            "A. Unity in Diversity",
+            "B. Faster, Higher, Stronger",
+            "C. Friendship and Fair Play",
+            "D. Excellence and Achievement"
+        ]
+    },
+    {
+        numb: 4,
+        question: "In which year were the first Winter Olympics held?",
+        answer: "B. 1924",
+        options: [
+            "A. 1900",
+            "B. 1924",
+            "C. 1948",
+            "D. 1952"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which country hosted the 2016 Summer Olympics?",
+        answer: "B. Brazil",
+        options: [
+            "A. China",
+            "B. Brazil",
+            "C. Canada",
+            "D. Russia"
+        ]
+    }
+];
